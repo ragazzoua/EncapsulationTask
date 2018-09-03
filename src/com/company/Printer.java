724a00442d257c5bed3de5ae8c7a41c1c6b697cd
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-09-03 18:22:06 +0300

    create some methods

---
+++ src/com/company/Printer.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/**
+ * created by s.a.miroshnychenko 9/3/2018
+ */
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmmount) {
+        if (tonerAmmount > 0 && tonerAmmount <= 100) {
+            if (this.tonerLevel + tonerAmmount > 100) {
+                return -1;
+            }
+            this.tonerLevel += tonerAmmount;
+            return this.tonerLevel;
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint /= 2;
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+}
